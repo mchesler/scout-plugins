02acfaf798a0c0120cc290009f46c5178613a72b
Matt Chesler <mchesler@movableink.com>
2016-09-08 16:51:34 -0400

    Separate output by protocol

--- nstat/nstat.rb
+++ nstat/nstat.rb
@@ -13,14 +13,197 @@ class NStat < Scout::Plugin
   class BinaryNotFoundError < RuntimeError; end
   class InvalidOutputError < RuntimeError; end
 
+  KEYS = {
+    tcp: [
+      :TcpActiveOpens,
+      :TcpPassiveOpens,
+      :TcpAttemptFails,
+      :TcpEstabResets,
+      :TcpInSegs,
+      :TcpOutSegs,
+      :TcpRetransSegs,
+      :TcpInErrs,
+      :TcpOutRsts,
+      :TcpInCsumErrors
+    ],
+    udp: [
+      :UdpInDatagrams,
+      :UdpNoPorts,
+      :UdpInErrors,
+      :UdpOutDatagrams,
+      :UdpRcvbufErrors,
+      :UdpSndbufErrors,
+      :UdpInCsumErrors,
+      :UdpLiteInDatagrams,
+      :UdpLiteNoPorts,
+      :UdpLiteInErrors,
+      :UdpLiteOutDatagrams,
+      :UdpLiteRcvbufErrors,
+      :UdpLiteSndbufErrors,
+      :UdpLiteInCsumErrors
+    ],
+    udp6: [
+      :Udp6InDatagrams,
+      :Udp6NoPorts,
+      :Udp6InErrors,
+      :Udp6OutDatagrams,
+      :Udp6RcvbufErrors,
+      :Udp6SndbufErrors,
+      :Udp6InCsumErrors,
+      :UdpLite6InDatagrams,
+      :UdpLite6NoPorts,
+      :UdpLite6InErrors,
+      :UdpLite6OutDatagrams,
+      :UdpLite6RcvbufErrors,
+      :UdpLite6SndbufErrors,
+      :UdpLite6InCsumErrors
+    ],
+    ip: [
+      :IpInReceives,
+      :IpInHdrErrors,
+      :IpInAddrErrors,
+      :IpForwDatagrams,
+      :IpInUnknownProtos,
+      :IpInDiscards,
+      :IpInDelivers,
+      :IpOutRequests,
+      :IpOutDiscards,
+      :IpOutNoRoutes,
+      :IpReasmTimeout,
+      :IpReasmReqds,
+      :IpReasmOKs,
+      :IpReasmFails,
+      :IpFragOKs,
+      :IpFragFails,
+      :IpFragCreates
+    ],
+    ip6: [
+      :Ip6InReceives,
+      :Ip6InHdrErrors,
+      :Ip6InTooBigErrors,
+      :Ip6InNoRoutes,
+      :Ip6InAddrErrors,
+      :Ip6InUnknownProtos,
+      :Ip6InTruncatedPkts,
+      :Ip6InDiscards,
+      :Ip6InDelivers,
+      :Ip6OutForwDatagrams,
+      :Ip6OutRequests,
+      :Ip6OutDiscards,
+      :Ip6OutNoRoutes
+    ],
+    ip6ext: [
+      :Ip6ReasmTimeout,
+      :Ip6ReasmReqds,
+      :Ip6ReasmOKs,
+      :Ip6ReasmFails,
+      :Ip6FragOKs,
+      :Ip6FragFails,
+      :Ip6FragCreates,
+      :Ip6InMcastPkts,
+      :Ip6OutMcastPkts,
+      :Ip6InOctets,
+      :Ip6OutOctets,
+      :Ip6InMcastOctets,
+      :Ip6OutMcastOctets,
+      :Ip6InBcastOctets,
+      :Ip6OutBcastOctets,
+      :Ip6InNoECTPkts,
+      :Ip6InECT1Pkts,
+      :Ip6InECT0Pkts,
+      :Ip6InCEPkts
+    ],
+    icmpin: [
+      :IcmpInMsgs,
+      :IcmpInErrors,
+      :IcmpInCsumErrors,
+      :IcmpInDestUnreachs,
+      :IcmpInTimeExcds,
+      :IcmpInParmProbs,
+      :IcmpInSrcQuenchs,
+      :IcmpInRedirects,
+      :IcmpInEchos,
+      :IcmpInEchoReps,
+      :IcmpInTimestamps,
+      :IcmpInTimestampReps,
+      :IcmpInAddrMasks,
+      :IcmpInAddrMaskReps
+    ],
+    icmpout: [
+      :IcmpOutMsgs,
+      :IcmpOutErrors,
+      :IcmpOutDestUnreachs,
+      :IcmpOutTimeExcds,
+      :IcmpOutParmProbs,
+      :IcmpOutSrcQuenchs,
+      :IcmpOutRedirects,
+      :IcmpOutEchos,
+      :IcmpOutEchoReps,
+      :IcmpOutTimestamps,
+      :IcmpOutTimestampReps,
+      :IcmpOutAddrMasks,
+      :IcmpOutAddrMaskReps
+    ],
+    icmp6in: [
+      :Icmp6InMsgs,
+      :Icmp6InErrors,
+      :Icmp6InCsumErrors,
+      :Icmp6InDestUnreachs,
+      :Icmp6InPktTooBigs,
+      :Icmp6InTimeExcds,
+      :Icmp6InParmProblems,
+      :Icmp6InEchos,
+      :Icmp6InEchoReplies,
+      :Icmp6InGroupMembQueries,
+      :Icmp6InGroupMembResponses,
+      :Icmp6InGroupMembReductions,
+      :Icmp6InRouterSolicits,
+      :Icmp6InRouterAdvertisements,
+      :Icmp6InNeighborSolicits,
+      :Icmp6InNeighborAdvertisements,
+      :Icmp6InRedirects,
+      :Icmp6InMLDv2Reports
+    ],
+    icmp6out: [
+      :Icmp6OutMsgs,
+      :Icmp6OutErrors,
+      :Icmp6OutDestUnreachs,
+      :Icmp6OutPktTooBigs,
+      :Icmp6OutTimeExcds,
+      :Icmp6OutParmProblems,
+      :Icmp6OutEchos,
+      :Icmp6OutEchoReplies,
+      :Icmp6OutGroupMembQueries,
+      :Icmp6OutGroupMembResponses,
+      :Icmp6OutGroupMembReductions,
+      :Icmp6OutRouterSolicits,
+      :Icmp6OutRouterAdvertisements,
+      :Icmp6OutNeighborSolicits,
+      :Icmp6OutNeighborAdvertisements,
+      :Icmp6OutRedirects,
+      :Icmp6OutMLDv2Reports,
+      :Icmp6OutType133,
+      :Icmp6OutType135,
+      :Icmp6OutType143
+    ]
+  }
+
+  PROTOCOLS = KEYS.keys.map{|k| k.to_s}
+
   OPTIONS=<<-EOS
     path:
-      name: nstat path
+      name: nstat Path
       default: /usr/bin/nstat
       notes: Location to find nstat if it's not on the path
+    protocol:
+      name: Protocol
+      default: udp
+      notes: Protocol to collect from #{PROTOCOLS.join(', ')}
   EOS
 
   def build_report
+    error("Invalid protocol specified", option(:protocol)) unless PROTOCOLS.include?(option(:protocol).to_s.downcase)
+
     process_nstat_output(execute_command)
 
     report(@data)
@@ -32,7 +215,7 @@ class NStat < Scout::Plugin
 
   protected
   def nstat_executable
-    @nstat_executable = option(:path).to_s.empty? ? "nstat": File.join(option(:path), "nstat")
+    @nstat_executable = option(:path).to_s.empty? ? "/usr/bin/nstat" : option(:path)
   end
 
   def nstat_bin
@@ -52,7 +235,8 @@ class NStat < Scout::Plugin
     raise(InvalidOutputError, results[:output].chomp) if results[:exit_code] != 0
 
     begin
-      @data = JSON.parse(results[:output], symbolize_names: true)[:kernel]
+      data = JSON.parse(results[:output], symbolize_names: true)[:kernel]
+      @data = data.select {|k,v| KEYS[option(:protocol).downcase.to_sym].include?(k)}
     rescue JSON::JSONError => e
       raise(InvalidOutputError, e.message)
     end
--- nstat/test.rb
+++ nstat/test.rb
@@ -4,7 +4,7 @@ require File.expand_path('../nstat.rb', __FILE__)
 class NStatTest < Test::Unit::TestCase
 
   def setup
-    @plugin = NStat.new(nil, {}, { path: '/usr/bin' })
+    @plugin = NStat.new(nil, {}, { path: '/usr/bin', protocol: 'udp' })
   end
 
   def test_successful_run_with_no_errors
@@ -47,5 +47,85 @@ class NStatTest < Test::Unit::TestCase
     assert_equal [], res[:alerts]
     assert res[:reports].first.is_a?(Hash)
     assert_equal 10115891, res[:reports].first[:UdpInErrors]
+  end
+
+  def test_invalid_protocol
+    plugin = NStat.new(nil, {}, { path: '/usr/bin', protocol: 'gibberish' })
+
+    res = plugin.run()
+    assert_equal "Invalid protocol specified", res[:errors].first[:subject]
+    assert_equal "gibberish", res[:errors].first[:body]
+  end
+
+  def test_tcp
+    plugin = NStat.new(nil, {}, { path: '/usr/bin', protocol: 'tcp' })
+    plugin.stubs(:execute_command).returns({ exit_code: 0, output: File.read(File.dirname(__FILE__)+'/fixtures/sample.json') })
+
+    res = plugin.run()
+    assert res[:reports].first.has_key?(:TcpActiveOpens)
+  end
+
+  def test_udp
+    plugin = NStat.new(nil, {}, { path: '/usr/bin', protocol: 'udp' })
+    plugin.stubs(:execute_command).returns({ exit_code: 0, output: File.read(File.dirname(__FILE__)+'/fixtures/sample.json') })
+
+    res = plugin.run()
+    assert res[:reports].first.has_key?(:UdpInDatagrams)
+  end
+
+  def test_udp6
+    plugin = NStat.new(nil, {}, { path: '/usr/bin', protocol: 'udp6' })
+    plugin.stubs(:execute_command).returns({ exit_code: 0, output: File.read(File.dirname(__FILE__)+'/fixtures/sample.json') })
+
+    res = plugin.run()
+    assert res[:reports].first.has_key?(:Udp6InDatagrams)
+  end
+
+  def test_ip
+    plugin = NStat.new(nil, {}, { path: '/usr/bin', protocol: 'ip' })
+    plugin.stubs(:execute_command).returns({ exit_code: 0, output: File.read(File.dirname(__FILE__)+'/fixtures/sample.json') })
+
+    res = plugin.run()
+    assert res[:reports].first.has_key?(:IpInReceives)
+  end
+
+  def test_ip6
+    plugin = NStat.new(nil, {}, { path: '/usr/bin', protocol: 'ip6' })
+    plugin.stubs(:execute_command).returns({ exit_code: 0, output: File.read(File.dirname(__FILE__)+'/fixtures/sample.json') })
+
+    res = plugin.run()
+    assert res[:reports].first.has_key?(:Ip6InReceives)
+  end
+
+  def test_icmpin
+    plugin = NStat.new(nil, {}, { path: '/usr/bin', protocol: 'icmpin' })
+    plugin.stubs(:execute_command).returns({ exit_code: 0, output: File.read(File.dirname(__FILE__)+'/fixtures/sample.json') })
+
+    res = plugin.run()
+    assert res[:reports].first.has_key?(:IcmpInMsgs)
+  end
+
+  def test_icmpout
+    plugin = NStat.new(nil, {}, { path: '/usr/bin', protocol: 'icmpout' })
+    plugin.stubs(:execute_command).returns({ exit_code: 0, output: File.read(File.dirname(__FILE__)+'/fixtures/sample.json') })
+
+    res = plugin.run()
+    assert res[:reports].first.has_key?(:IcmpOutMsgs)
+  end
+
+  def test_icmp6in
+    plugin = NStat.new(nil, {}, { path: '/usr/bin', protocol: 'icmp6in' })
+    plugin.stubs(:execute_command).returns({ exit_code: 0, output: File.read(File.dirname(__FILE__)+'/fixtures/sample.json') })
+
+    res = plugin.run()
+    assert res[:reports].first.has_key?(:Icmp6InMsgs)
+  end
+
+  def test_icmp6out
+    plugin = NStat.new(nil, {}, { path: '/usr/bin', protocol: 'icmp6out' })
+    plugin.stubs(:execute_command).returns({ exit_code: 0, output: File.read(File.dirname(__FILE__)+'/fixtures/sample.json') })
+
+    res = plugin.run()
+    assert res[:reports].first.has_key?(:Icmp6OutMsgs)
   end
 end
